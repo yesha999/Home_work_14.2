67b03a47fe005952013f632a453a43df8a969c42
yesha999 <scrolltrip@mail.ru>
2022-02-20 16:24:07 +0300

    Выполнение ДЗ ver. 1.0

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
+import json
 import sqlite3
 
-__all__ = ['search_title']
+__all__ = ['search_title', 'search_years', 'search_rating', 'search_genre']
 
 
 def search_title(title):
@@ -19,3 +20,124 @@ def search_title(title):
             found_result = result
             break
     return found_result
+
+
+def search_years(year_1, year_2):
+    year_1 = int(year_1)
+    year_2 = int(year_2)
+    connection = sqlite3.connect("netflix.db")
+    cursor = connection.cursor()
+    found_results = []
+    search_years_query = ("""
+        SELECT title, release_year
+        FROM netflix
+        WHERE release_year BETWEEN ? AND ?
+        ORDER BY release_year DESC
+        LIMIT 100
+        """)
+    cursor.execute(search_years_query, (year_1, year_2))
+    results = cursor.fetchall()
+    connection.close()
+    for result in results:
+        found_result = {'title': result[0], 'release_year': result[1]}
+        found_results.append(found_result)
+    return found_results
+
+
+def search_rating(rating):
+    connection = sqlite3.connect("netflix.db")
+    cursor = connection.cursor()
+    found_results = []
+    search_rating_query = ("""
+        SELECT title, rating, description
+        FROM netflix 
+        WHERE rating != '' AND (rating=? OR rating=? OR rating=?)
+        LIMIT 100
+        """)
+    if rating == 'children':
+        cursor.execute(search_rating_query, ('G', 'G', 'G'))  # ну костыльно и шо :)
+        results = cursor.fetchall()
+    elif rating == 'adult':
+        cursor.execute(search_rating_query, ('R', 'RC-17', 'R'))
+        results = cursor.fetchall()
+    elif rating == 'family':
+        cursor.execute(search_rating_query, ('G', 'PG', 'PG-13'))
+        results = cursor.fetchall()
+    else:
+        results = []
+    connection.close()
+
+    for result in results:
+        found_result = {'title': result[0], 'rating': result[1], 'description': result[2]}
+        found_results.append(found_result)
+
+    return found_results
+
+
+def search_genre(genre):
+    connection = sqlite3.connect("netflix.db")
+    cursor = connection.cursor()
+    found_results = []
+    search_genre_query = ("""
+    SELECT title, description, release_year
+    FROM netflix
+    WHERE listed_in LIKE '%'||?||'%'
+    ORDER BY release_year DESC
+    LIMIT 10
+    """)
+    cursor.execute(search_genre_query, (genre,))
+    results = cursor.fetchall()
+    connection.close()
+    for result in results:
+        found_result = {'title': result[0], 'description': result[1], }
+        found_results.append(found_result)
+    return found_results
+
+
+def step_five_function(actor_1, actor_2):
+    """Напишите функцию, которая получает в качестве аргумента имена двух актеров,
+    сохраняет всех актеров из колонки cast и возвращает список тех, кто играет с ними в паре больше 2 раз.
+    Для этого задания не требуется создавать вьюшку
+    В качестве теста можно передать: Rose McIver и Ben Lamb, Jack Black и Dustin Hoffman."""
+    connection = sqlite3.connect("netflix.db")
+    cursor = connection.cursor()
+    search_actor_query = ("""
+        SELECT "cast"
+        FROM netflix
+        WHERE "cast" LIKE '%'||?||'%' AND "cast" LIKE '%'||?||'%'
+        """)
+    cursor.execute(search_actor_query, (actor_1, actor_2))
+    results = cursor.fetchall()
+    connection.close()
+    actors = []
+    for result in results:
+        for actor_cast in result:
+            actor_cast_list = actor_cast.split(', ')
+            for actor in actor_cast_list:
+                if actor != actor_1 and actor != actor_2:
+                    actors.append(actor)
+    actor_friends = [actor for actor in set(actors) if actors.count(actor) > 2]
+    return actor_friends
+
+
+print(step_five_function('Rose McIver', 'Ben Lamb'))
+
+
+def step_six_sql(type, release_year, listed_in):
+    connection = sqlite3.connect("netflix.db")
+    cursor = connection.cursor()
+    found_results = []
+    search_all_query = ("""
+    SELECT title, description
+    FROM netflix
+    WHERE type LIKE '%'||?||'%' AND release_year LIKE '%'||?||'%' AND listed_in LIKE '%'||?||'%'
+    """)
+    cursor.execute(search_all_query, (type, release_year, listed_in))
+    results = cursor.fetchall()
+    connection.close()
+    for result in results:
+        found_result = {'title': result[0], 'description': result[1]}
+        found_results.append(found_result)
+    with open('step_6.json', 'w') as file:
+        json.dump(found_results, file)
+    return found_results
--- main.py
+++ main.py
@@ -1,22 +1,61 @@
-import sqlite3
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from functions import *
 
-
 app = Flask('__name__')
 
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
 
 @app.route('/movie/<title>')
 def title_page(title):
     search_title(title)
     return render_template('movie_title_page.html', movie=search_title(title))
 
-@app.route('/movie/year/to/year')
-def year_to_year_page():
-    pass
 
+@app.route('/movie/year/to/year', methods=["GET"])
+def year_to_year_form():
+    return render_template('year_to_year_form.html')
+
+
+@app.route('/movie/year/to/year', methods=["POST"])
+def year_to_year_page():
+    year_1 = request.values.get("year_1")
+    year_2 = request.values.get("year_2")
+    if year_1.isdigit() and year_2.isdigit():
+        found_results = search_years(year_1, year_2)
+        return render_template("/year_to_year_page.html", year_1=year_1, year_2=year_2, movies=found_results)
+    return redirect("/movie/year/to/year")
+
+
+@app.route('/rating')
+def rating_page():
+    return render_template('rating_choice.html')
+
+
+@app.route('/rating/<rating>')
+def rating_page_chosen(rating):
+    movies = search_rating(rating)
+    movies_count = len(movies)
+    if rating == 'children':
+        header = 'для детей'
+    elif rating == 'adult':
+        header = 'для взрослых'
+    elif rating == 'family':
+        header = 'для семейного просмотра'
+    else:
+        return redirect("/rating")
+    return render_template('rating_page_chosen.html', movies=movies,
+                           header=header, movies_count=movies_count)
+
+
+@app.route('/genre/<genre>')
+def genre_page(genre):
+    movies = search_genre(genre)
+    movies_count = len(movies)
+    return render_template('genre_page.html', movies=movies,
+                           movies_count=movies_count, genre=genre)
 
 
 if __name__ == '__main__':
